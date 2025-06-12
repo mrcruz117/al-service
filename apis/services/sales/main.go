e6b726cdcb19e114049462069c8fd75fadf8de9c
Michael Cruz <mcruz@aersale.com>
2025-06-12 11:02:16 -0400

    basic log hw

--- apis/services/sales/main.go
+++ apis/services/sales/main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"context"
 	"os"
+	"os/signal"
+	"runtime"
+	"syscall"
 
 	"github.com/mrcruz117/al-service/foundation/logger"
 )
@@ -20,9 +23,10 @@ func main() {
 		return "" //web.GetTraceID(ctx)
 	}
 
-	log = logger.NewWithEvents(os.Stdout, logger.LevelError, "SALES", traceIDFn, events)
+	log = logger.NewWithEvents(os.Stdout, logger.LevelInfo, "SALES", traceIDFn, events)
 
 	// ------------------------------------------------------------
+
 	ctx := context.Background()
 
 	if err := run(ctx, log); err != nil {
@@ -33,6 +37,18 @@ func main() {
 }
 
 func run(ctx context.Context, log *logger.Logger) error {
+	// ------------------------------------------------------------
+	// GOMAXPROCS
+
+	log.Info(ctx, "startup", "GOMAXPROCS", runtime.GOMAXPROCS(0))
+
+	shutdown := make(chan os.Signal, 1)
+
+	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
+	sig := <-shutdown
+
+	log.Info(ctx, "shutdown", "status", "shutdown started", "signal", sig)
+	defer log.Info(ctx, "shutdown", "status", "shutdown complete", "signal", sig)
 
 	return nil
 }
---
+++ makefile
@@ -0,0 +1,6 @@
+
+SHELL_PATH = /bin/ash
+SHELL = $(if $(wildcard $(SHELL_PATH)),/bin/ash,/bin/bash)
+
+run:
+	go run apis/services/sales/main.go
