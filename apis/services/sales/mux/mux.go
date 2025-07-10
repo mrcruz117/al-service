7bf511620521492587ed851cd057bc53876465cb
mrcruz117 <mrcruz1989@gmail.com>
2025-07-09 21:53:09 -0400

    'custom' mux

--- apis/services/sales/mux/mux.go
+++ apis/services/sales/mux/mux.go
@@ -3,14 +3,15 @@
 package mux
 
 import (
-	"net/http"
+	"os"
 
 	"github.com/mrcruz117/al-service/apis/services/sales/route/sys/checkapi"
+	"github.com/mrcruz117/al-service/foundation/web"
 )
 
 // WebAPI constructs an http.Handler with all the application routes bound
-func WebAPI() http.Handler {
-	mux := http.NewServeMux()
+func WebAPI(shutdown chan os.Signal) *web.App {
+	mux := web.NewApp(shutdown)
 
 	checkapi.Routes(mux)
 
