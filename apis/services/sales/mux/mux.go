7344e84451f03689d7718c3c6316dea85ce0ab4d
mrcruz117 <mrcruz1989@gmail.com>
2025-07-15 21:16:57 -0400

    panic mw

--- apis/services/sales/mux/mux.go
+++ apis/services/sales/mux/mux.go
@@ -13,7 +13,7 @@ import (
 
 // WebAPI constructs an http.Handler with all the application routes bound
 func WebAPI(log *logger.Logger, shutdown chan os.Signal) *web.App {
-	mux := web.NewApp(shutdown, mid.Logger(log), mid.Errors(log))
+	mux := web.NewApp(shutdown, mid.Logger(log), mid.Errors(log), mid.Panics())
 
 	checkapi.Routes(mux)
 
