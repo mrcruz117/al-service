3f39aced73ee5fd6f9ecdf302f5e235cb6aa424f
Michael Cruz <mcruz@aersale.com>
2025-07-17 17:38:49 -0400

    generating jwt

--- apis/tooling/admin/main.go
+++ apis/tooling/admin/main.go
@@ -8,13 +8,127 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v4"
 )
 
 func main() {
-	err := GenKey()
+	err := GenToken()
 	if err != nil {
 		log.Fatalln(err)
 	}
+}
+
+func GenToken() error {
+
+	// Generating a token requires defining a set of claims. In this applications
+	// case, we only care about defining the subject and the user in question and
+	// the roles they have on the database. This token will expire in a year.
+	//
+	// iss (issuer): Issuer of the JWT
+	// sub (subject): Subject of the JWT (the user)
+	// aud (audience): Recipient for which the JWT is intended
+	// exp (expiration time): Time after which the JWT expires
+	// nbf (not before time): Time before which the JWT must not be accepted for processing
+	// iat (issued at time): Time at which the JWT was issued; can be used to determine age of the JWT
+	// jti (JWT ID): Unique identifier; can be used to prevent the JWT from being replayed (allows a token to be used only once)
+	claims := struct {
+		jwt.RegisteredClaims
+		Roles []string
+	}{
+		RegisteredClaims: jwt.RegisteredClaims{
+			Subject:   "38dc9d84-018b-4a15-b958-0b78af11c301",
+			Issuer:    "service project",
+			ExpiresAt: jwt.NewNumericDate(time.Now().UTC().Add(8760 * time.Hour)), // 1 year
+			IssuedAt:  jwt.NewNumericDate(time.Now().UTC()),
+		},
+		Roles: []string{"ADMIN"},
+	}
+
+	method := jwt.GetSigningMethod(jwt.SigningMethodRS256.Name)
+	token := jwt.NewWithClaims(method, claims)
+
+	// kid is key ID
+	token.Header["kid"] = "54bb2165-71e1-41a6-af3e-7da4a0e1e2c1"
+
+	privateKeyPEM, err := os.ReadFile("zarf/keys/54bb2165-71e1-41a6-af3e-7da4a0e1e2c1.pem")
+	if err != nil {
+		return fmt.Errorf("reading private pem: %w", err)
+	}
+
+	privateKey, err := jwt.ParseRSAPrivateKeyFromPEM(privateKeyPEM)
+	if err != nil {
+		return fmt.Errorf("parsing private pem: %w", err)
+	}
+
+	str, err := token.SignedString(privateKey)
+	if err != nil {
+		return fmt.Errorf("signing token: %w", err)
+	}
+
+	fmt.Printf("-----BEGIN TOKEN-----\n%s\n-----END TOKEN-----\n\n", str)
+
+	// -------------------------------------------------------------------------
+
+	// Marshal the public key from the private key to PKIX.
+	asn1Bytes, err := x509.MarshalPKIXPublicKey(&privateKey.PublicKey)
+	if err != nil {
+		return fmt.Errorf("marshaling public key: %w", err)
+	}
+
+	// Construct a PEM block for the public key.
+	publicBlock := pem.Block{
+		Type:  "PUBLIC KEY",
+		Bytes: asn1Bytes,
+	}
+
+	// Write the public key to the public key file.
+	if err := pem.Encode(os.Stdout, &publicBlock); err != nil {
+		return fmt.Errorf("encoding to public file: %w", err)
+	}
+
+	// var b bytes.Buffer
+	// if err := pem.Encode(&b, &publicBlock); err != nil {
+	// 	return fmt.Errorf("encoding to public file: %w", err)
+	// }
+
+	// -------------------------------------------------------------------------
+
+	// ctx := context.Background()
+	// query := fmt.Sprintf("x = data.%s.%s", "ardan.rego", "auth")
+
+	// q, err := rego.New(
+	// 	rego.Query(query),
+	// 	rego.Module("policy.rego", opaAuthentication),
+	// ).PrepareForEval(ctx)
+	// if err != nil {
+	// 	return err
+	// }
+
+	// input := map[string]any{
+	// 	"Key":   b.String(),
+	// 	"Token": str,
+	// 	"ISS":   "service project",
+	// }
+
+	// results, err := q.Eval(ctx, rego.EvalInput(input))
+	// if err != nil {
+	// 	return fmt.Errorf("query: %w", err)
+	// }
+
+	// if len(results) == 0 {
+	// 	return errors.New("no results")
+	// }
+
+	// result, ok := results[0].Bindings["x"].(bool)
+	// if !ok || !result {
+	// 	return fmt.Errorf("bindings results[%v] ok[%v]", results, ok)
+	// }
+
+	// fmt.Println("\nTOKEN VALIDATED!")
+
+	return nil
 }
 
 // GenKey creates an x509 private/public key for auth tokens.
