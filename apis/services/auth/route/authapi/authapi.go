3393a2eb68c3de9cefb5886d51bf863fbee7ebf1
Michael Cruz <mcruz@aersale.com>
2026-01-23 11:05:30 -0500

    authclient models

--- apis/services/auth/route/authapi/authapi.go
+++ apis/services/auth/route/authapi/authapi.go
@@ -5,7 +5,7 @@ import (
 	"context"
 	"net/http"
 
-	"github.com/google/uuid"
+	"github.com/mrcruz117/al-service/app/api/authclient"
 	"github.com/mrcruz117/al-service/app/api/errs"
 	"github.com/mrcruz117/al-service/app/api/mid"
 	"github.com/mrcruz117/al-service/business/api/auth"
@@ -54,10 +54,7 @@ func (api *api) authenticate(ctx context.Context, w http.ResponseWriter, r *http
 		return errs.New(errs.Unauthenticated, err)
 	}
 
-	resp := struct {
-		UserID uuid.UUID
-		Claims auth.Claims
-	}{
+	resp := authclient.AuthenticateResp{
 		UserID: userID,
 		Claims: mid.GetClaims(ctx),
 	}
@@ -66,11 +63,7 @@ func (api *api) authenticate(ctx context.Context, w http.ResponseWriter, r *http
 }
 
 func (api *api) authorize(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
-	var auth struct {
-		Claims auth.Claims
-		UserID uuid.UUID
-		Rule   string
-	}
+	var auth authclient.Authorize
 	if err := web.Decode(r, &auth); err != nil {
 		return errs.New(errs.FailedPrecondition, err)
 	}
---
+++ app/api/authclient/authclient.go
@@ -0,0 +1,2 @@
+// Package authclient provides support to access the auth service.
+package authclient
